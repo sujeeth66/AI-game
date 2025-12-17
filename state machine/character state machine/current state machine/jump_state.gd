b1f7757a2b7ec47bb96d225d60b9620adf32cccb
sujeeth66 <ganjisujeeth5@gmail.com>
2025-12-17 12:50:05 +0530

    empty commit

--- state machine/character state machine/current state machine/jump_state.gd
+++ state machine/character state machine/current state machine/jump_state.gd
@@ -23,68 +23,71 @@ func enter():
 	
 	
 func physics_update(delta):
-	print(character.velocity)
-	# Get input direction with deadzone
 	var input_direction = Input.get_axis("move_left", "move_right")
-	
+	var is_touching_wall = character.is_on_wall_only()
+	var is_jumping = Input.is_action_pressed("jump")
+	var is_jump_just_pressed = Input.is_action_just_pressed("jump")
+
+	# Update drag timer
 	if GlobalStates.is_wall_jumping and drag_timer < drag_cooldown:
 		GlobalStates.is_wall_jumping = false
-		
 	drag_timer += delta
-	
-	if character.is_on_wall_only() and not input_direction:
+
+	# Wall detection
+	if is_touching_wall and not character.is_on_floor():
 		wall_collided = true
-		
-	if wall_collided == true:
 		can_wall_jump = true
-		if not character.is_on_floor() :
-			if not input_direction:
-				
-				character.velocity = Vector2i(0,20)
-			else:
-				character.velocity.x = direction * 200 * abs(input_direction)
-		else:
-			wall_collided = false
 	else:
+		wall_collided = false
 		can_wall_jump = false
-		character.velocity.x = direction * 200 * abs(input_direction)
-			
-	# Only update direction if it's different from current direction
-	if input_direction != 0 :
-		var new_direction = sign(input_direction)
-		if new_direction != 0 and new_direction != sign(direction):
-			direction = new_direction
-	
-	GlobalStates.facing_right = direction > 0
-	_update_sprite_direction()
-	
-	# Variable jump height - if jump button is released early, reduce upward velocity
-	if not Input.is_action_pressed("jump") and character.velocity.y < 0:
+
+	# Handle wall jump
+	if can_wall_jump and is_jump_just_pressed and wall_jump_timer > wall_jump_cooldown:
+		GlobalStates.is_wall_jumping = true
+		GlobalStates.jump_count += 1
+		wall_jump_timer = 0
+
+		# Flip direction away from wall
+		var wall_dir = character.get_last_slide_collision().normal.x
+		direction = -sign(wall_dir)
+
+		# Apply wall jump velocity
+		character.velocity.x = direction * 250
+		character.velocity.y = JUMP_FORCE * 0.8  # Slightly weaker than ground jump
+
+		animated_sprite.play("jump")
+		_update_sprite_direction()
+		return  # Skip rest of update to preserve jump impulse
+
+	# Regular horizontal movement
+	if input_direction != 0:
+		direction = sign(input_direction)
+		character.velocity.x = direction * 200
+	else:
+		character.velocity.x = 0
+
+	# Variable jump height
+	if not is_jumping and character.velocity.y < 0:
 		character.velocity.y *= JUMP_RELEASE_REDUCTION
-	
-	# Only allow dashing if we have enough stamina
+
+	# Dash
 	if Input.is_action_just_pressed("dash") and character.has_stamina(character.STAMINA_DASH_COST):
 		state_machine.change_state("dashstate")
 		return
-	#print(wall_jump_timer)
-	if  wall_jump_timer > wall_jump_cooldown :
-		if can_wall_jump and GlobalStates.jump_count < jump_limit and Input.is_action_pressed("jump"):
-			GlobalStates.is_wall_jumping = true
-			GlobalStates.jump_count += 1
-			state_machine.change_state("jumpstate")
-	wall_jump_timer += delta
-	
-	# Check if landed
+
+	# Landing
 	if character.is_on_floor():
 		GlobalStates.jump_count = 0
-		if Input.is_action_pressed("jump"):
+		if is_jumping:
 			state_machine.change_state("jumpstate")
-		elif input_direction != 0:  
+		elif input_direction != 0:
 			state_machine.change_state("runstate")
 		else:
 			state_machine.change_state("idlestate")
-	
+
+	# Apply gravity and movement
 	character.move_and_slide()
+	wall_jump_timer += delta
 	
 func exit():
 	animated_sprite.stop()
