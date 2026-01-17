06fd4fed617f0a031cf3e688c18ffd98408b977c
sujeeth66 <ganjisujeeth5@gmail.com>
2026-01-17 10:57:25 +0530

    more file cleaning

--- scripts/global.gd
+++ scripts/global.gd
@@ -15,21 +15,3 @@ var global_quest_ui : Control
 var global_quest_manager : Node2D
 var active_quests = {}
 var player: CharacterBody2D 
-
-var max_health: int = 1000
-var current_health: int = max_health
-
-# 🔋 Player Resources
-var current_stamina: float = 100.0
-var max_stamina: float = 100.0
-var current_mana: float = 100.0
-var max_mana: float = 100.0
-
-
-func _ready() -> void:
-	Engine.max_fps = 30  # Or 60, or whatever you want
-	# Create UI scene
-	call_deferred("_init_health_bar")
-	
-func _init_health_bar():
-	var health_bar = player.health_bar
